127f518f58aa0c3cae8ac18f37d6fc85d64f0c43
LasseFoxy <lafo0001@stud.kea.dk>
2024-09-19 10:57:25 +0200

    findAll()

--- src/main/java/kea/springstart/controller/RestController.java
+++ src/main/java/kea/springstart/controller/RestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
 import java.util.Optional;
 
 @org.springframework.web.bind.annotation.RestController
@@ -17,6 +18,11 @@ public class RestController {
     @GetMapping("/model/{name}")
     public Optional <Model>  getModelByName(@PathVariable String name) {
         return modelRepository.findByName(name);
+    }
+
+    @GetMapping("/model")
+    public List<Model> findAllModels() {
+        return modelRepository.findAll();
     }
 
 }
--- src/main/java/kea/springstart/modelRepo/ModelRepository.java
+++ src/main/java/kea/springstart/modelRepo/ModelRepository.java
@@ -2,9 +2,9 @@ package kea.springstart.modelRepo;
 
 import kea.springstart.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface ModelRepository extends JpaRepository<Model, Integer> {
     Optional<Model> findByName(String name);
+
 }
